0acc249fd6623cd02c62a0047d1b661191c2d50b
proxin <postm1@yandex.ru>
2023-10-31 14:18:49 +0300

    Grass win turn off fix

--- ClassLibrary1/Common/Utils.cs
+++ ClassLibrary1/Common/Utils.cs
@@ -14,7 +14,7 @@ namespace SpacerUnion
 {
     public class Constants
     {
-        public const string SPACER_VERSION = "0.42";
+        public const string SPACER_VERSION = "0.43";
 
 
         public const string FILE_FILTER_OPEN_ZEN = "Compiled ZEN (*.zen)|*.zen|Uncompiled ZEN (*.zen)|*.zen";
