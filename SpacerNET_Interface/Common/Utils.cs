8e14914920783ed70e2d8415f1730d56cfcf3c8b
proxin <postm1@yandex.ru>
2024-03-20 09:44:17 +0300

    Fix # char

--- SpacerNET_Interface/Common/Utils.cs
+++ SpacerNET_Interface/Common/Utils.cs
@@ -14,7 +14,7 @@ namespace SpacerUnion
 {
     public class Constants
     {
-        public const string SPACER_VERSION = "1.02";
+        public const string SPACER_VERSION = "1.03";
 
 
         public const string FILE_FILTER_OPEN_ZEN = "Compiled ZEN (*.zen)|*.zen|Uncompiled ZEN (*.zen)|*.zen";
@@ -176,7 +176,7 @@ namespace SpacerUnion
 
         public static bool IsOnlyLatin(string text)
         {
-            return Regex.IsMatch(text, @"^[a-zA-Z0-9_\-\+\=\.\,\;\:\\\/\(\)\]\[ ]+$");
+            return Regex.IsMatch(text, @"^[a-zA-Z0-9_\-\+\=\.\,\;\:\\\/\(\)\]\[\# ]+$");
         }
 
         public static bool IsOptionActive(string text)
