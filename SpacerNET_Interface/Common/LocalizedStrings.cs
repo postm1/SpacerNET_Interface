5c4b50adce11c6f9f95c6c30215682f50cab53d9
proxin <postm1@yandex.ru>
2024-04-09 11:53:58 +0300

    Lang add

--- SpacerNET_Interface/Common/LocalizedStrings.cs
+++ SpacerNET_Interface/Common/LocalizedStrings.cs
@@ -755,8 +755,8 @@ namespace SpacerUnion.Common
             words.Add("VOB_FILTER_IGNORE_PFX", new List<string> { "Игнорировать PFX", "Ignore PFX", "PFX Ignorieren", "Ignoruj PFX" });
             words.Add("KEYS_FAST_FILTER_IGNOREPFX", new List<string> { "Выбрать фильтр выделения вобов: Игнорировать PFX", "Choose vob filter selection mod: Ignore PFX", "Wähle Vob-Filter Selektierungs-Modus: PFX Ignorieren", "Wybór trybu filtrowania voba: Ignoruj ​​PFX" });
 
-            words.Add("MSG_WRONG_COLOR_FORMAT", new List<string> { "Недопустимый формат цвета! Используйте формат RGBA '255 255 255 255'", "Wrong color format! Format is RGBA '255 255 255 255'", "", "" });
-            words.Add("MSG_WRONG_COLOR_FORMAT_RANGE", new List<string> { "Допустимый диапазон цвета от 0 до 255!", "Valid color range is 0 to 255 ", "", "" });
+            words.Add("MSG_WRONG_COLOR_FORMAT", new List<string> { "Недопустимый формат цвета! Используйте формат RGBA '255 255 255 255'", "Wrong color format! Format is RGBA '255 255 255 255'", "Falsches Farbformat! Format ist RGBA '255 255 255 255'", "" });
+            words.Add("MSG_WRONG_COLOR_FORMAT_RANGE", new List<string> { "Допустимый диапазон цвета от 0 до 255!", "Valid color range is 0 to 255 ", "Gültiger Farbbereich ist 0 bis 255", "Dopuszczalny zakres koloru jest od wartości 0 do 255!" });
         }
         
     }
