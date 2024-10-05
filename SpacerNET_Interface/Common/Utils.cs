293793f31f5e3644c7fbecb9899d7d498f02cfc8
proxin <postm1@yandex.ru>
2024-10-05 16:21:43 +0300

    1.22 version change

--- SpacerNET_Interface/Common/Utils.cs
+++ SpacerNET_Interface/Common/Utils.cs
@@ -15,7 +15,7 @@ namespace SpacerUnion
 {
     public class Constants
     {
-        public const string SPACER_VERSION = "1.21";
+        public const string SPACER_VERSION = "1.22";
 
 
         public const string FILE_FILTER_OPEN_ZEN = "Compiled ZEN (*.zen)|*.zen|Uncompiled ZEN (*.zen)|*.zen";
