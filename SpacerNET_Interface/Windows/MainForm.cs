3eb53b4ef84c738e77abf5d058e0bbbf1d145b84
Till Rathmann <till.rathmann@gmx.de>
2026-02-08 13:29:16 +0100

    Fix mouse focus issue when clicking tool windows

--- SpacerNET_Interface/Windows/MainForm.cs
+++ SpacerNET_Interface/Windows/MainForm.cs
@@ -2113,6 +2113,22 @@ namespace SpacerUnion
                 
             }
 
+            // additional check: is the mouse over a visible tool-window?
+            if (result == 0)
+            {
+                foreach (Form window in SpacerNET.windowsList)
+                {
+                    if (window.Visible)
+                    {
+                        Rectangle windowRect = new Rectangle(window.Location, window.Size);
+                        if (windowRect.Contains(Cursor.Position))
+                        {
+                            result = 1;
+                            break;
+                        }
+                    }
+                }
+            }
 
             return result;
         }
