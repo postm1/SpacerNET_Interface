4a04d6a62be3a7ba6feb59a91a89838c9567c073
proxin <postm1@yandex.ru>
2026-05-01 11:14:09 +0300

    Logging: add load/save vobtree

--- SpacerNET_Interface/Windows/ObjTree.cs
+++ SpacerNET_Interface/Windows/ObjTree.cs
@@ -1275,6 +1275,11 @@ namespace SpacerUnion
                 string filePath = saveFileDialogVobTree.FileName;
                 Imports.Stack_PushString(filePath);
                 Imports.Extern_SaveVobTree();
+
+                string fileNameForInfo = Path.GetFileName(filePath);
+
+                SpacerNET.form.AddText("[->] " + Localizator.Get("UNION_VOBTREE_SAVE") + " | " + fileNameForInfo, Color.FromArgb(255, 255, 0, 0));
+
             }
             
         }
@@ -1337,6 +1342,10 @@ namespace SpacerUnion
                 Imports.Stack_PushString(filePath);
                 Imports.Extern_OpenVobTree(false, insertNearCamera);
 
+                string fileNameForInfo = Path.GetFileName(filePath);
+
+                SpacerNET.form.AddText("[+] " + Localizator.Get("UNION_VOBTREE_INSERT") + " | " + fileNameForInfo, Color.FromArgb(255, 255, 0, 0));
+
             }
 
             
@@ -1392,6 +1401,9 @@ namespace SpacerUnion
                     SpacerNET.objTreeWin.globalTree.SelectedNode.Expand();
                 }
 
+                string fileNameForInfo = Path.GetFileName(filePath);
+
+                SpacerNET.form.AddText("[+] " + Localizator.Get("UNION_VOBTREE_INSERT") + " | " + fileNameForInfo, Color.FromArgb(255, 255, 0, 0));
             }
             
         }
